2d464a170af470c3904a985461203729ec780cf6
Matt Addison <matthew.addison@talk21.com>
2018-10-08 16:29:19 +0100

    pass drink test in waterbottle class

---
+++ src/main/java/WaterBottle.java
@@ -0,0 +1,16 @@
+public class WaterBottle {
+    private int volume;
+
+    public WaterBottle(int volume){
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+    public int drink() {
+        this.volume =  this.volume - 10;
+        return this.volume;
+    }
+}
---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+    @Test
+    public void drinkingLowersVolume(){
+        assertEquals(90, waterBottle.drink());
+    }
+}
